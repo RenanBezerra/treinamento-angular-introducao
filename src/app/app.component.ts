48c64bccaeced8aaf21afc0c81d2a822bd542a53
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-12-11 17:19:43 -0300

    2.5
    trabalhando com o ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'treinamento-angular';
   nome = '';
+  lista = [100,200,400,230,1];
 
   validar(){
     console.log(this.nome)
