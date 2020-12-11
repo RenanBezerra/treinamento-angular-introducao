9016582d2b5ac105b4f515ce1494d14576f2807e
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-12-11 17:04:36 -0300

    2.4
    usando ngIf para condições e lembrar de none e hidden que dependendo da estratégia podem servir melhor que o if , porque o if pode ser mais custoso para exibir na pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'treinamento-angular';
-  nome = 'Nome';
+  nome = '';
 
   validar(){
     console.log(this.nome)
