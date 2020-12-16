55df74c02a2a90d94a5c58ca6837448894029a76
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-12-16 11:29:42 -0300

    3.2
    Entendendo o reactive-forms e a validção do formulario

--- src/app/formularios/reactive-forms/reactive-forms.component.ts
+++ src/app/formularios/reactive-forms/reactive-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormsComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      nomeDoUsuario: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      idade: ['', [Validators.required, Validators.min(0)]],
+    });
+  }
+
+  salvar(){
+    console.log(this.form)
+    if (this.form.valid){
+      
+    }
   }
 
 }
